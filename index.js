74e107372def6c81c6ca7107343b4305fe20f147
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-11 19:25:57 -0700

    Ejemplo 2

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ if (time < 0 || time >= 24 || typeof time == "number") {
 }
 
 console.log(greeting);
+
+let day = 5;
+let msg;
+
+switch (day) {
+    case 0:
+        msg = 'Sunday';
+        break;
+    case 1:
+        msg = 'Monday';
+        break;
+    case 2:
+        msg = "Tuesday";
+        break;
+    case 3:
+        msg = "Wednesday";
+        break;
+    case 4:
+        msg = "Thursday";
+        break;
+    case 5:
+        msg = "Friday";
+        break;
+    case 6:
+        msg = "Saturday";
+        break;
+    default:
+        msg = "Error";
+}
+
+console.log(msg)
