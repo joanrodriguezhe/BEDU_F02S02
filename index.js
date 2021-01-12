f6c2e3f6d6d4a6a5a0ad9aaf489b981b949a3344
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-11 18:56:10 -0700

    Reto 1

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let time = 13;
+let time = 34;
 let greeting;
-
-if (time < 12) {
+if (time < 0 || time >= 24 || typeof time == "number") {
+    greeting = 'Invalid time';
+} else if (time < 12) {
     greeting = 'Good morning';
 } else if (time < 20) {
     greeting = 'Good afternoon';
